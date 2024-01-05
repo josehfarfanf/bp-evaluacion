a0d176e7e2c159241417f377efe93ff662e0efeb
jfarfan <jfarfan@pichincha.com>
2024-01-05 16:40:22 +0000

    init commit

---
+++ login/src/main/java/com/pichincha/chapter/evaluacion/login/LoginApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.chapter.evaluacion.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginApplication.class, args);
+	}
+
+}
